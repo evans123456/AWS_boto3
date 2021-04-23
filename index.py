9ce71189de3ee0930c065e278a942b1f3ba61780
evans123456 <evansgichuki656@gmail.com>
2021-04-23 05:07:38 +0100

    finished the UI

--- index.py
+++ index.py
@@ -2,31 +2,62 @@ from flask import Flask, redirect,url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def index(name):
-    return render_template("index.html",content=name, r= 2)
 
-@app.route("/login",methods = ["POST","GET"])
-def login():
+
+@app.route("/",methods = ["POST","GET"])
+def home():
     if request.method == "POST":
-        user = request.form["nm"]
-        print(user)
-        return redirect(url_for("user",usr=user))
+        # user = request.form["nm"]
+        service = request.form["flexRadioDefault"]
+        print("The service: ",service)
+        return redirect(url_for("resources",srv=service))
 
         
     else:
-        return render_template("login.html")
-
-
-@app.route("/<usr>")
-def user(usr):
-    return render_template("user.html",content="evans")
+        return render_template("home.html")
 
 
 
-@app.route("/adm")
-def adm():
-    return redirect(url_for("user",name="Admin!"))
+@app.route("/output")
+def output():
+    return render_template("output.html")
+
+
+
+
+
+@app.route("/input/lastpage",methods = ["POST","GET"])
+def lastPage():
+    if request.method == "POST":
+        s = request.form["number_of_shots"]
+        q = request.form["reporting_rate"]
+        d = request.form["matching_digits"]
+        print(f"S : {s}, Q: {q},D : {d}")
+        return redirect(url_for("output"))
+
+        
+    else:
+        return render_template("lastpage.html")
+    
+
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+
+@app.route("/<srv>",methods = ["POST","GET"])
+def resources(srv):
+    if request.method == "POST":
+        no_of_resources = request.form["no_of_resource"]
+        print("Resources: ",no_of_resources)
+        return redirect(url_for("lastPage"))
+
+        
+    else:
+        return render_template("resources.html",content=srv)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- lambdainstance.py
+++ lambdainstance.py
@@ -1,3 +1,27 @@
+'''using AWS lambda, we can run our code without caring about the servers. Normally if you want to run your application
+you will need virtual machines for that e.g ec2 instance. When we get ec2 instance we normally install python/java and our code runs
+above that. Using AWS lamba we excecute the code without worrying about servers.
+Lambda is also known as serverless architecture( we are running code on those servers but we are not managing those servers)
+
+Benefits include
+- no servers to manage
+- better performance executing code via lambda(runs on latest generation computers)
+- Auto scales (based on the number of functions comming to the page)
+- we only pay when our code executes(not when idle)
+
+Lambda Use cases
+- in response to events e.g 
+--- file uploaded to s3 -> process the file and store data into dynamo db
+--- create thumbnails for images uploaded to s3
+--- resize images uploaded to s3
+--- transcode images automatically
+--- automation of EBS snapshots
+- serverless architecture (web dvlpt can be done with lambda help)
+
+current use case. When ec2 service stops we send the instance id email to mngment
+lambda will need access to SNS and cloudwatch logs
+'''
+
 import boto3
 
 client = boto3.client('lambda')
